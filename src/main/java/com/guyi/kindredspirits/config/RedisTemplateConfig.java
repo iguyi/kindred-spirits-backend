62d4ba9c5b928c382027eb735047a43b94dca3aa
guyi <362312391@qq.com>
2023-11-08 21:35:51 +0800

    update 数据全部以 string 方式存储

--- src/main/java/com/guyi/kindredspirits/config/RedisTemplateConfig.java
+++ src/main/java/com/guyi/kindredspirits/config/RedisTemplateConfig.java
@@ -8,21 +8,33 @@ import org.springframework.data.redis.serializer.RedisSerializer;
 
 /**
  * Redis 模板配置
+ *
+ * @author 张仕恒
  */
 @Configuration
 public class RedisTemplateConfig {
     /**
      * 自定义序列化器
      *
-     * @param connectionFactory - Redis 连接工厂
-     * @return
+     * @param connectionFactory - Redis 连接工厂, Spring 自动注入
+     * @return redisTemplate
      */
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(connectionFactory);  // 设置连接工厂
-        // 对于数据的 key, 使用 StringRedisSerializer 序列化器,
-        redisTemplate.setKeySerializer(RedisSerializer.string());
+        // 设置连接工厂
+        redisTemplate.setConnectionFactory(connectionFactory);
+
+        RedisSerializer<String> redisStringSerializer = RedisSerializer.string();
+
+        // 设置 key 和 hashKey 的序列化器
+        redisTemplate.setKeySerializer(redisStringSerializer);
+        redisTemplate.setHashKeySerializer(redisStringSerializer);
+
+        // 设置 value 和 hashValue 的序列化器
+        redisTemplate.setValueSerializer(redisStringSerializer);
+        redisTemplate.setHashValueSerializer(redisStringSerializer);
+
         return redisTemplate;
     }
 }
