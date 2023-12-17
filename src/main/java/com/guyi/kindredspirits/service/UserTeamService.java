947ee2def4816457eb0860e1e8576da1c2f5ac09
guyi <362312391@qq.com>
2023-12-17 22:18:48 +0800

    根据用户 id 查询 “用户-队伍” 信息

--- src/main/java/com/guyi/kindredspirits/service/UserTeamService.java
+++ src/main/java/com/guyi/kindredspirits/service/UserTeamService.java
@@ -3,11 +3,21 @@ package com.guyi.kindredspirits.service;
 import com.guyi.kindredspirits.model.domain.UserTeam;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
 * @author 张仕恒
 * @description 针对表【user_team(用户-队伍关系表)】的数据库操作Service
 * @createDate 2023-10-01 21:57:09
 */
 public interface UserTeamService extends IService<UserTeam> {
+
+    /**
+     * 根据用户 id 查询 “用户-队伍” 信息
+     *
+     * @param userId - 用户 id
+     * @return userId 对应的 “用户-队伍“ 信息列表
+     */
+    List<UserTeam> getMessageByUserId(Long userId);
 
 }
--- src/main/java/com/guyi/kindredspirits/service/impl/UserTeamServiceImpl.java
+++ src/main/java/com/guyi/kindredspirits/service/impl/UserTeamServiceImpl.java
@@ -1,10 +1,13 @@
 package com.guyi.kindredspirits.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.guyi.kindredspirits.model.domain.UserTeam;
 import com.guyi.kindredspirits.service.UserTeamService;
 import com.guyi.kindredspirits.mapper.UserTeamMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
 * @author 张仕恒
@@ -12,8 +15,14 @@ import org.springframework.stereotype.Service;
 * @createDate 2023-10-01 21:57:09
 */
 @Service
-public class UserTeamServiceImpl extends ServiceImpl<UserTeamMapper, UserTeam>
-    implements UserTeamService{
+public class UserTeamServiceImpl extends ServiceImpl<UserTeamMapper, UserTeam> implements UserTeamService{
+
+    @Override
+    public List<UserTeam> getMessageByUserId(Long userId) {
+        QueryWrapper<UserTeam> userTeamQueryWrapper = new QueryWrapper<>();
+        userTeamQueryWrapper.eq("userId", userId);
+        return this.list(userTeamQueryWrapper);
+    }
 
 }
 
