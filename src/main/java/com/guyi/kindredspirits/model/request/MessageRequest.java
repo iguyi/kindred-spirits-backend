4c90b5723711bc5a61afb5578a48f9a917381de6
guyi <362312391@qq.com>
2023-12-27 21:14:26 +0800

    消息封装类(请求)

---
+++ src/main/java/com/guyi/kindredspirits/model/request/MessageRequest.java
@@ -0,0 +1,45 @@
+package com.guyi.kindredspirits.model.request;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 消息封装类
+ *
+ * @author 孤诣
+ */
+@Data
+public class MessageRequest implements Serializable {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 消息发送者 id
+     */
+    private Long senderId;
+
+    /**
+     * 消息接受者 id
+     */
+    private Long receiverId;
+
+    /**
+     * 消息类型:
+     * 0 - 系统消息(好友申请通过、入队申请通过等)
+     * 1 - 验证消息(比如好友申请、入队申请等)
+     * 2 - 消息通知(需要系统处理的消息)
+     */
+    private Integer messageType;
+
+    /**
+     * 消息主体(内容)
+     */
+    private String messageBody;
+
+    private static final long serialVersionUID = 1L;
+
+}
