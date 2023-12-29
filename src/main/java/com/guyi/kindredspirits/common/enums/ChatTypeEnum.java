17e4807886d7ec0b7e732a40aa9e1faf02e3fb1f
guyi <362312391@qq.com>
2023-12-29 23:16:06 +0800

    聊天类型枚举

---
+++ src/main/java/com/guyi/kindredspirits/common/enums/ChatTypeEnum.java
@@ -0,0 +1,57 @@
+package com.guyi.kindredspirits.common.enums;
+
+/**
+ * 聊天类型枚举
+ *
+ * @author 孤诣
+ */
+public enum ChatTypeEnum {
+
+    /**
+     * 私聊
+     */
+    PRIVATE_CHAT(0, "私聊"),
+
+    /**
+     * 群聊(队内聊天)
+     */
+    GROUP_CHAT(1, "群聊");
+
+    public static ChatTypeEnum getEnumByType(Integer type) {
+        if (type == null) {
+            return null;
+        }
+        ChatTypeEnum[] chatTypeEnums = ChatTypeEnum.values();
+        for (ChatTypeEnum chatTypeEnum : chatTypeEnums) {
+            if (chatTypeEnum.getType().equals(type)) {
+                return chatTypeEnum;
+            }
+        }
+        return null;
+    }
+
+    Integer type;
+    String text;
+
+    ChatTypeEnum(Integer type, String text) {
+        this.type = type;
+        this.text = text;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}
