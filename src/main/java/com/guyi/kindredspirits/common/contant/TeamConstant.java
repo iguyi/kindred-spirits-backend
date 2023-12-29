ab700215a01ed61841adf2f92dd8475f3df08d1f
guyi <362312391@qq.com>
2023-12-29 22:13:46 +0800

    队伍枚举类

---
+++ src/main/java/com/guyi/kindredspirits/common/contant/TeamConstant.java
@@ -0,0 +1,30 @@
+package com.guyi.kindredspirits.common.contant;
+
+/**
+ * 队伍枚举类
+ *
+ * @author 孤诣
+ */
+public @interface TeamConstant {
+
+    /**
+     * 队伍最少人数限制
+     */
+    Integer MINX_NUMBER_PEOPLE = 3;
+
+    /**
+     * 队伍最多人数限制
+     */
+    Integer MAX_NUMBER_PEOPLE = 10;
+
+    /**
+     * 队伍名称最大长度限制
+     */
+    Integer MAX_TEAM_NAME_LENGTH = 10;
+
+    /**
+     * 队伍描述长度最大限制
+     */
+    Integer MAX_DESCRIPTION_LENGTH = 512;
+
+}
