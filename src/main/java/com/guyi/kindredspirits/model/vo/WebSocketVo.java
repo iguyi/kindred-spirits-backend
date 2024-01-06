ec9cbe2b1d7f2b59c6078836813341f0e19c2407
guyi <362312391@qq.com>
2024-01-07 00:14:40 +0800

    用户 WebSocket 签名

---
+++ src/main/java/com/guyi/kindredspirits/model/vo/WebSocketVo.java
@@ -0,0 +1,23 @@
+package com.guyi.kindredspirits.model.vo;
+
+import lombok.Data;
+
+/**
+ * 用户 WebSocket 签名
+ *
+ * @author 孤诣
+ */
+@Data
+public class WebSocketVo {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 账号
+     */
+    private String userAccount;
+
+}
