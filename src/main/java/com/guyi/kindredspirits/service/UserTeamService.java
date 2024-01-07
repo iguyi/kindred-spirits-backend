ad53a64b88154ee6ce5c2a212e849d433b1f1477
guyi <1802366955@qq.com>
2024-01-07 22:15:18 +0800

    根据队伍 id 查询 "用户-队伍" 信息

--- src/main/java/com/guyi/kindredspirits/service/UserTeamService.java
+++ src/main/java/com/guyi/kindredspirits/service/UserTeamService.java
@@ -1,23 +1,31 @@
 package com.guyi.kindredspirits.service;
 
-import com.guyi.kindredspirits.model.domain.UserTeam;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.guyi.kindredspirits.model.domain.UserTeam;
 
 import java.util.List;
 
 /**
-* @author 孤诣
-* @description 针对表【user_team(用户-队伍关系表)】的数据库操作Service
-* @createDate 2023-10-01 21:57:09
-*/
+ * 针对表 user_team(用户-队伍关系表) 的数据库操作 Service
+ *
+ * @author 孤诣
+ */
 public interface UserTeamService extends IService<UserTeam> {
 
     /**
-     * 根据用户 id 查询 “用户-队伍” 信息
+     * 根据用户 id 查询 "用户-队伍" 信息
      *
      * @param userId - 用户 id
      * @return userId 对应的 “用户-队伍“ 信息列表
      */
     List<UserTeam> getMessageByUserId(Long userId);
+
+    /**
+     * 根据队伍 id 查询 "用户-队伍" 信息
+     *
+     * @param teamId - 队伍 id
+     * @return teamId 对应的 “用户-队伍“ 信息列表
+     */
+    List<UserTeam> getMessageByTeamId(Long teamId);
 
 }
--- src/main/java/com/guyi/kindredspirits/service/impl/UserTeamServiceImpl.java
+++ src/main/java/com/guyi/kindredspirits/service/impl/UserTeamServiceImpl.java
@@ -2,25 +2,32 @@ package com.guyi.kindredspirits.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.guyi.kindredspirits.mapper.UserTeamMapper;
 import com.guyi.kindredspirits.model.domain.UserTeam;
 import com.guyi.kindredspirits.service.UserTeamService;
-import com.guyi.kindredspirits.mapper.UserTeamMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
-* @author 孤诣
-* @description 针对表【user_team(用户-队伍关系表)】的数据库操作Service实现
-* @createDate 2023-10-01 21:57:09
-*/
+ * 针对表 user_team(用户-队伍关系表) 的数据库操作 Service 实现
+ *
+ * @author 孤诣
+ */
 @Service
-public class UserTeamServiceImpl extends ServiceImpl<UserTeamMapper, UserTeam> implements UserTeamService{
+public class UserTeamServiceImpl extends ServiceImpl<UserTeamMapper, UserTeam> implements UserTeamService {
 
     @Override
     public List<UserTeam> getMessageByUserId(Long userId) {
         QueryWrapper<UserTeam> userTeamQueryWrapper = new QueryWrapper<>();
         userTeamQueryWrapper.eq("userId", userId);
+        return this.list(userTeamQueryWrapper);
+    }
+
+    @Override
+    public List<UserTeam> getMessageByTeamId(Long teamId) {
+        QueryWrapper<UserTeam> userTeamQueryWrapper = new QueryWrapper<>();
+        userTeamQueryWrapper.eq("teamId", teamId);
         return this.list(userTeamQueryWrapper);
     }
 
