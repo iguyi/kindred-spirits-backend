9825470158d9780db1f50467ee34406ea6bca02b
guyi <1802366955@qq.com>
2024-01-17 23:23:24 +0800

    查询队伍请求响应封装

---
+++ src/main/java/com/guyi/kindredspirits/model/vo/TeamVo.java
@@ -0,0 +1,68 @@
+package com.guyi.kindredspirits.model.vo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 查询队伍请求响应封装
+ *
+ * @author 孤诣
+ */
+@Data
+public class TeamVo implements Serializable {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 队伍名称
+     */
+    private String name;
+
+    /**
+     * 队伍描述
+     */
+    private String description;
+
+    /**
+     * 队伍最大人数
+     */
+    private Integer maxNum;
+
+    /**
+     * 过期时间
+     */
+    private Date expireTime;
+
+    /**
+     * 创建人 id
+     */
+    private Long userId;
+
+    /**
+     * 队长 id
+     */
+    private Long leaderId;
+
+    /**
+     * 0 - 公开，1 - 私有，2 - 加密
+     */
+    private Integer status;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    private static final long serialVersionUID = -43278945587191896L;
+
+}
