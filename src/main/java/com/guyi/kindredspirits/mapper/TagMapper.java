b0472c531876a21f4d4291747abe364d64127c36
guyi <1802366955@qq.com>
2024-01-31 20:28:33 +0800

    补充注释信息

--- src/main/java/com/guyi/kindredspirits/mapper/TagMapper.java
+++ src/main/java/com/guyi/kindredspirits/mapper/TagMapper.java
@@ -4,8 +4,10 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.guyi.kindredspirits.model.domain.Tag;
 
 /**
-* @author 孤诣
-*/
+ * 针对表 tag(标签表) 的数据库操作 Mapper
+ *
+ * @author 孤诣
+ */
 public interface TagMapper extends BaseMapper<Tag> {
 
 }
