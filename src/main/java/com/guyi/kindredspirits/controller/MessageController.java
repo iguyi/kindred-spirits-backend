42ed0e0fdea0a64d6a7b1913505caed013e81aba
guyi <1802366955@qq.com>
2024-01-31 20:22:13 +0800

    清理多余的 import

--- src/main/java/com/guyi/kindredspirits/controller/MessageController.java
+++ src/main/java/com/guyi/kindredspirits/controller/MessageController.java
@@ -11,7 +11,6 @@ import com.guyi.kindredspirits.model.vo.MessageVo;
 import com.guyi.kindredspirits.service.MessageService;
 import com.guyi.kindredspirits.service.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
