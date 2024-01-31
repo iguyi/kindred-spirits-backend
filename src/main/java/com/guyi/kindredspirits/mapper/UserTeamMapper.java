9b30baefc3962175816196e9901119472afc46e2
guyi <1802366955@qq.com>
2024-01-31 20:30:25 +0800

    补充注释信息

--- src/main/java/com/guyi/kindredspirits/mapper/UserTeamMapper.java
+++ src/main/java/com/guyi/kindredspirits/mapper/UserTeamMapper.java
@@ -4,11 +4,10 @@ import com.guyi.kindredspirits.model.domain.UserTeam;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 /**
-* @author 孤诣
-* @description 针对表【user_team(用户-队伍关系表)】的数据库操作Mapper
-* @createDate 2023-10-01 21:57:09
-* @Entity com.guyi.kindredspirits.model.domain.UserTeam
-*/
+ * 针对表 user_team(用户-队伍关系表) 的数据库操作 Mapper
+ *
+ * @author 孤诣
+ */
 public interface UserTeamMapper extends BaseMapper<UserTeam> {
 
 }
