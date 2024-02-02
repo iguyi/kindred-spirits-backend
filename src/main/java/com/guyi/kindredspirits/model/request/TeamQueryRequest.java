2892304ef7320cce2ccbb278c5549531245e7f05
guyi <1802366955@qq.com>
2024-02-02 14:43:50 +0800

    完善 serialVersionUID

--- src/main/java/com/guyi/kindredspirits/model/request/TeamQueryRequest.java
+++ src/main/java/com/guyi/kindredspirits/model/request/TeamQueryRequest.java
@@ -14,6 +14,7 @@ import java.io.Serializable;
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class TeamQueryRequest extends PageRequest implements Serializable {
+
     /**
      * id
      */
@@ -54,5 +55,6 @@ public class TeamQueryRequest extends PageRequest implements Serializable {
      */
     private Integer status;
 
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = -4532186854643391018L;
+
 }
