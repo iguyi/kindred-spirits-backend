fbcedbe67900b7be499370abd7a27832dabf57b6
guyi <1802366955@qq.com>
2024-02-02 15:44:53 +0800

    补充返回值

--- src/main/java/com/guyi/kindredspirits/util/lock/LockCallback.java
+++ src/main/java/com/guyi/kindredspirits/util/lock/LockCallback.java
@@ -1,13 +1,16 @@
 package com.guyi.kindredspirits.util.lock;
 
 /**
+ * @param <T> 任务执行结果的类型
  * @author 孤诣
  */
-public interface LockCallback {
+public interface LockCallback<T> {
 
     /**
      * LockUtil 类中的回调方法, 用于编写任务逻辑
+     *
+     * @return 处理结果
      */
-    void execute();
+    T execute();
 
 }
