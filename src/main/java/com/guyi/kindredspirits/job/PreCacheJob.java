b3745888ab16598ff81f902223b69ec8bb17c4e5
guyi <1802366955@qq.com>
2024-02-02 15:26:37 +0800

    使用自定义 LockUtil, 提高代码可读性

--- src/main/java/com/guyi/kindredspirits/job/PreCacheJob.java
+++ src/main/java/com/guyi/kindredspirits/job/PreCacheJob.java
@@ -8,10 +8,10 @@ import com.guyi.kindredspirits.mapper.UserMapper;
 import com.guyi.kindredspirits.model.domain.User;
 import com.guyi.kindredspirits.service.UserService;
 import com.guyi.kindredspirits.util.AlgorithmUtil;
+import com.guyi.kindredspirits.util.lock.LockUtil;
 import com.guyi.kindredspirits.util.redis.RedisUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -53,60 +53,55 @@ public class PreCacheJob {
         // 获取锁对象
         final String lockKey = String.format(RedisConstant.KEY_PRE, "precache-job", "do-cache", "lock");
 
-        // 获取锁对象
-        RLock lock = redissonClient.getLock(lockKey);
-        try {
-            // 尝试获取锁, 没获取到锁就不执行(等待)了
-            if (lock.tryLock(0, RedisConstant.SCHEDULED_LOCK_LEASE_TIME, TimeUnit.SECONDS)) {
-                // 查询所有热点用户数据
-                QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
-                userQueryWrapper.select("id", "userAccount", "username", "avatarUrl", "gender", "tags", "email",
-                                "phone", "profile")
-                        .eq("isHot", UserConstant.HOT_USER_TAG);
-                List<User> mainUserList = userMapper.selectList(userQueryWrapper);
+        LockUtil.opsRedissonLock(lockKey, 0, RedisConstant.SCHEDULED_LOCK_LEASE_TIME, TimeUnit.SECONDS,
+                redissonClient, this::cacheRecommendUser);
+    }
 
-                for (User mainUser : mainUserList) {
-                    // 查询所有用户信息
-                    userQueryWrapper = new QueryWrapper<>();
-                    userQueryWrapper.select("id", "userAccount", "username", "avatarUrl", "gender", "tags", "profile"
-                            , "phone", "email");
-                    userQueryWrapper.ne("id", mainUser.getId());
-                    userQueryWrapper.isNotNull("tags");
-                    List<User> userList = userService.list(userQueryWrapper);
+    /**
+     * 缓存为人点用户推荐的用户
+     */
+    private void cacheRecommendUser() {
+        // 查询所有热点用户数据
+        QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
+        userQueryWrapper
+                .select("id", "userAccount", "username", "avatarUrl", "gender", "tags", "email", "phone", "profile")
+                .eq("isHot", UserConstant.HOT_USER_TAG);
+        List<User> mainUserList = userMapper.selectList(userQueryWrapper);
 
-                    Map<String, List<Integer>> loginUserTagMap = userService.getTagWeightList(mainUser.getTags());
-                    List<User> cacheUserList = new ArrayList<>();
-                    for (User user : userList) {
-                        String userTags = user.getTags();
-                        // 排除未设置标签用户和自己
-                        if (StringUtils.isBlank(userTags) || mainUser.getId().equals(user.getId())) {
-                            continue;
-                        }
-                        Map<String, List<Integer>> otherUserTagMap = userService.getTagWeightList(user.getTags());
-                        double similarity = AlgorithmUtil.similarity(loginUserTagMap, otherUserTagMap);
-                        // 相似度大于 0.7 才认为二者相似
-                        if (similarity > 0.7) {
-                            cacheUserList.add(user);
-                            user.setTags(userService.getTagListJson(user));
-                        }
-                    }
+        for (User mainUser : mainUserList) {
+            // 查询所有用户信息
+            userQueryWrapper = new QueryWrapper<>();
+            userQueryWrapper
+                    .select("id", "userAccount", "username", "avatarUrl", "gender", "tags", "profile", "phone", "email")
+                    .ne("id", mainUser.getId())
+                    // todo IS NOT NULL 会一起索引失效问题
+                    .isNotNull("tags");
+            List<User> userList = userService.list(userQueryWrapper);
 
-                    // 写缓存, key 超时时间 = 15 小时 + 随机时间(分钟)
-                    // todo 缓存问题
-                    String redisKey = String.format(RedisConstant.RECOMMEND_KEY_PRE, mainUser.getId());
-                    long timeout = RedisConstant.PRECACHE_TIMEOUT + RandomUtil.randomLong(15 * 60L);
-                    boolean result = RedisUtil.setValue(redisKey, cacheUserList, timeout, TimeUnit.MINUTES);
-                    if (!result) {
-                        log.error("缓存设置失败");
-                    }
+            Map<String, List<Integer>> loginUserTagMap = userService.getTagWeightList(mainUser.getTags());
+            List<User> cacheUserList = new ArrayList<>();
+            for (User user : userList) {
+                String userTags = user.getTags();
+                // 排除未设置标签用户和自己
+                if (StringUtils.isBlank(userTags) || mainUser.getId().equals(user.getId())) {
+                    continue;
+                }
+                Map<String, List<Integer>> otherUserTagMap = userService.getTagWeightList(user.getTags());
+                double similarity = AlgorithmUtil.similarity(loginUserTagMap, otherUserTagMap);
+                // 相似度大于 0.7 才认为二者相似
+                if (similarity > 0.7) {
+                    cacheUserList.add(user);
+                    user.setTags(userService.getTagListJson(user));
                 }
             }
-        } catch (InterruptedException e) {
-            log.debug("The doCacheRecommendUser method of the PreCacheJob class is error: " + e);
-        } finally {
-            // 只释放当前线程加的锁
-            if (lock.isHeldByCurrentThread()) {
-                lock.unlock();
+
+            // 写缓存, key 超时时间 = 15 小时 + 随机时间(分钟)
+            // todo 缓存问题
+            String redisKey = String.format(RedisConstant.RECOMMEND_KEY_PRE, mainUser.getId());
+            long timeout = RedisConstant.PRECACHE_TIMEOUT + RandomUtil.randomLong(15 * 60L);
+            boolean result = RedisUtil.setValue(redisKey, cacheUserList, timeout, TimeUnit.MINUTES);
+            if (!result) {
+                log.error("缓存设置失败");
             }
         }
     }
