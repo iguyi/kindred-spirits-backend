99c93f904399ff388c2f695ead8e21e2d713cab0
guyi <1802366955@qq.com>
2024-02-03 13:59:15 +0800

    获取锁失败后会重试

--- src/main/java/com/guyi/kindredspirits/util/lock/LockUtil.java
+++ src/main/java/com/guyi/kindredspirits/util/lock/LockUtil.java
@@ -1,5 +1,6 @@
 package com.guyi.kindredspirits.util.lock;
 
+import com.guyi.kindredspirits.common.contant.BaseConstant;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
@@ -18,7 +19,7 @@ public class LockUtil {
     }
 
     /**
-     * Redisson 实现 Redis 分布式锁
+     * Redisson 实现 Redis 分布式锁, 只尝试获取锁 1 次
      *
      * @param lockKey        - 锁对应的 key
      * @param leaseTime      - 持有锁的时间
@@ -27,7 +28,7 @@ public class LockUtil {
      * @param callback       - 回调函数, 用于定义需要执行的任务
      */
     public static <T> T opsRedissonLock(String lockKey, long waitTime, long leaseTime, TimeUnit timeUnit,
-                                       RedissonClient redissonClient, LockCallback<T> callback) {
+                                        RedissonClient redissonClient, LockCallback<T> callback) {
 
         T executeResult = null;
 
@@ -35,6 +36,41 @@ public class LockUtil {
         try {
             if (lock.tryLock(waitTime, leaseTime, timeUnit)) {
                 executeResult = callback.execute();
+            }
+        } catch (InterruptedException e) {
+            log.debug("{}#opsRedissonLock 发生异常, 异常信息如下: \n", LockUtil.class.getName(), e);
+        } finally {
+            // 只释放当前线程加的锁
+            if (lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            }
+        }
+        return executeResult;
+    }
+
+    /**
+     * Redisson 实现 Redis 分布式锁, 会尝试重新获取锁
+     *
+     * @param lockKey        - 锁对应的 key
+     * @param leaseTime      - 持有锁的时间
+     * @param timeUnit       - 时间单位
+     * @param redissonClient - Redisson 客户端
+     * @param callback       - 回调函数, 用于定义需要执行的任务
+     */
+    public static <T> T opsRedissonLockRetries(String lockKey, long waitTime, long leaseTime, TimeUnit timeUnit,
+                                               RedissonClient redissonClient, LockCallback<T> callback) {
+
+        T executeResult = null;
+
+        RLock lock = redissonClient.getLock(lockKey);
+        try {
+            int counter = 0;
+            while (counter < BaseConstant.RETRIES_MAX_NUMBER) {
+                if (lock.tryLock(waitTime, leaseTime, timeUnit)) {
+                    executeResult = callback.execute();
+                    break;
+                }
+                counter++;
             }
         } catch (InterruptedException e) {
             log.debug("{}#opsRedissonLock 发生异常, 异常信息如下: \n", LockUtil.class.getName(), e);
