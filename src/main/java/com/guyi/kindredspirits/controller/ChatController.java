e21d49dc87b9bbd596f15510d7d77834e4a85d9e
guyi <1802366955@qq.com>
2024-02-04 14:52:42 +0800

    为 Service 提供登录用户参数

--- src/main/java/com/guyi/kindredspirits/controller/ChatController.java
+++ src/main/java/com/guyi/kindredspirits/controller/ChatController.java
@@ -4,14 +4,17 @@ import com.guyi.kindredspirits.common.BaseResponse;
 import com.guyi.kindredspirits.common.ErrorCode;
 import com.guyi.kindredspirits.common.ResultUtils;
 import com.guyi.kindredspirits.exception.BusinessException;
+import com.guyi.kindredspirits.model.domain.User;
 import com.guyi.kindredspirits.model.request.ChatHistoryRequest;
 import com.guyi.kindredspirits.model.vo.ChatRoomVo;
 import com.guyi.kindredspirits.model.vo.ChatVo;
 import com.guyi.kindredspirits.service.ChatService;
+import com.guyi.kindredspirits.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -27,6 +30,9 @@ public class ChatController {
     @Resource
     private ChatService chatService;
 
+    @Resource
+    private UserService userService;
+
     /**
      * 获取私聊室的历史聊天记录
      *
@@ -34,11 +40,14 @@ public class ChatController {
      * @return 私聊室的历史聊天记录列表
      */
     @PostMapping("/private")
-    public BaseResponse<List<ChatVo>> getPrivateChat(@RequestBody ChatHistoryRequest chatHistoryRequest) {
+    public BaseResponse<List<ChatVo>> getPrivateChat(@RequestBody ChatHistoryRequest chatHistoryRequest,
+                                                     HttpServletRequest httpServletRequest) {
+        User loginUser = userService.getLoginUser(httpServletRequest);
+
         if (chatHistoryRequest == null) {
             throw new BusinessException(ErrorCode.NULL_ERROR, "请求参数为空");
         }
-        return ResultUtils.success(chatService.getPrivateChat(chatHistoryRequest));
+        return ResultUtils.success(chatService.getPrivateChat(loginUser, chatHistoryRequest));
     }
 
     /**
@@ -48,11 +57,14 @@ public class ChatController {
      * @return 队伍聊天室的历史聊天记录列表
      */
     @PostMapping("/team")
-    public BaseResponse<List<ChatVo>> getTeamChat(@RequestBody ChatHistoryRequest chatHistoryRequest) {
+    public BaseResponse<List<ChatVo>> getTeamChat(@RequestBody ChatHistoryRequest chatHistoryRequest,
+                                                  HttpServletRequest httpServletRequest) {
+        User loginUser = userService.getLoginUser(httpServletRequest);
+
         if (chatHistoryRequest == null) {
             throw new BusinessException(ErrorCode.NULL_ERROR, "请求参数为空");
         }
-        return ResultUtils.success(chatService.getTeamChat(chatHistoryRequest));
+        return ResultUtils.success(chatService.getTeamChat(loginUser, chatHistoryRequest));
     }
 
     /**
@@ -61,8 +73,9 @@ public class ChatController {
      * @return 历史聊天会话列表
      */
     @GetMapping("/room/list")
-    public BaseResponse<List<ChatRoomVo>> getChatRoomList() {
-        return ResultUtils.success(chatService.getChatRoomList());
+    public BaseResponse<List<ChatRoomVo>> getChatRoomList(HttpServletRequest httpServletRequest) {
+        User loginUser = userService.getLoginUser(httpServletRequest);
+        return ResultUtils.success(chatService.getChatRoomList(loginUser));
     }
 
 }
