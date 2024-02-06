208c8d4cea646df6b7e8676959053d1b4fe262d2
guyi <1802366955@qq.com>
2024-02-06 14:32:41 +0800

    根据队伍 id 统计队伍聊天数量

--- src/main/java/com/guyi/kindredspirits/mapper/ChatMapper.java
+++ src/main/java/com/guyi/kindredspirits/mapper/ChatMapper.java
@@ -2,6 +2,7 @@ package com.guyi.kindredspirits.mapper;
 
 import com.guyi.kindredspirits.model.domain.Chat;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * 针对表 chat(聊天记录表) 的数据库操作 Mapper
@@ -9,6 +10,14 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author 孤诣
  */
 public interface ChatMapper extends BaseMapper<Chat> {
+
+    /**
+     * 统计指定队伍中的聊天记录数
+     *
+     * @param teamId - 队伍 id
+     * @return 和指定队伍相关的聊天记录数
+     */
+    int countByTeamId(@Param("teamId") Long teamId);
 
 }
 
