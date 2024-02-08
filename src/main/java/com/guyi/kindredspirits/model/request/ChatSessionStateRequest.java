3bcd4ebaef681c885ffba2da329183986ebde165
guyi <1802366955@qq.com>
2024-02-08 23:54:19 +0800

    设置聊天会话状态请求

---
+++ src/main/java/com/guyi/kindredspirits/model/request/ChatSessionStateRequest.java
@@ -0,0 +1,33 @@
+package com.guyi.kindredspirits.model.request;
+
+import java.io.Serializable;
+
+/**
+ * 设置聊天会话状态请求
+ *
+ * @author 孤诣
+ */
+public class ChatSessionStateRequest implements Serializable {
+
+    /**
+     * 好友或队伍 id
+     */
+    private Long id;
+
+    /**
+     * 聊天会话的类型
+     *
+     * @see com.guyi.kindredspirits.common.enums.ChatTypeEnum
+     */
+    private Integer chatType;
+
+    /**
+     * 聊天会话状态, 聊天会话打开表示用户正在对应聊天窗口内<br/>
+     * - true: 聊天会话打开<br/>
+     * - false: 聊天会话关闭
+     */
+    private Boolean state;
+
+    private static final long serialVersionUID = 2903937319984483386L;
+
+}
