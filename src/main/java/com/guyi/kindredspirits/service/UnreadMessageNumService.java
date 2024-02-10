2bbcfd6af73bc1c66c6d215de994af371cdde167
guyi <1802366955@qq.com>
2024-02-10 12:08:19 +0800

    设置聊天会话状态请求

--- src/main/java/com/guyi/kindredspirits/service/UnreadMessageNumService.java
+++ src/main/java/com/guyi/kindredspirits/service/UnreadMessageNumService.java
@@ -2,6 +2,8 @@ package com.guyi.kindredspirits.service;
 
 import com.guyi.kindredspirits.model.domain.UnreadMessageNum;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.guyi.kindredspirits.model.domain.User;
+import com.guyi.kindredspirits.model.request.ChatSessionStateRequest;
 
 /**
  * 针对表 unread_message_num(未读聊天记录统计表) 的数据库操作 Service
@@ -9,5 +11,13 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @author 孤诣
  */
 public interface UnreadMessageNumService extends IService<UnreadMessageNum> {
+
+    /**
+     * 设置聊天会话状态请求
+     *
+     * @param loginUser    - 当前登录用户
+     * @param stateRequest - 设置聊天会话状态请求参数的封装
+     */
+    void setSessionSate(User loginUser, ChatSessionStateRequest stateRequest);
 
 }
