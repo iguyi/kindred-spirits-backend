94d776179172d7745eecc5f7c40dbe9c7245b2fe
guyi <1802366955@qq.com>
2024-02-14 16:36:00 +0800

    定时将 Redis 缓存的数据存入 MySQL

---
+++ src/main/java/com/guyi/kindredspirits/job/SaveCacheJob.java
@@ -0,0 +1,85 @@
+package com.guyi.kindredspirits.job;
+
+import com.guyi.kindredspirits.common.contant.RedisConstant;
+import com.guyi.kindredspirits.model.domain.UnreadMessageNum;
+import com.guyi.kindredspirits.service.UnreadMessageNumService;
+import com.guyi.kindredspirits.util.lock.LockUtil;
+import com.guyi.kindredspirits.util.redis.RedisUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.CollectionUtils;
+import org.redisson.api.RedissonClient;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 将 Redis 缓存中的数据存入 MySQL 中
+ *
+ * @author 孤诣
+ */
+@Component
+@Slf4j
+public class SaveCacheJob {
+
+    @Resource
+    private RedissonClient redissonClient;
+
+    @Resource
+    private UnreadMessageNumService unreadMessageNumService;
+
+    /**
+     * 将 Redis 缓存中的未读消息数消息写入 MySQL
+     * 每 5 分钟执行一次
+     */
+    @Scheduled(cron = "0 0/5 * * * *")
+    public void saveSessionSateJob() {
+        String lockKey = String.format(RedisConstant.LOCK_KEY, "save", "state", "unread");
+        LockUtil.opsRedissonLock(lockKey,
+                0,
+                RedisConstant.SCHEDULED_LOCK_LEASE_TIME,
+                TimeUnit.SECONDS,
+                redissonClient,
+                saveSessionState());
+    }
+
+    public Runnable saveSessionState() {
+        return () -> {
+            // 判断 key 是否存在
+            boolean hasRedisKey = RedisUtil.hasRedisKey(RedisConstant.SESSION_STATE_KEY_LIST);
+            if (!hasRedisKey) {
+                return;
+            }
+
+            // 获取所有未读(聊天)消息数据
+            List<String> sessionNameList = RedisUtil.STRING_REDIS_TEMPLATE.opsForList()
+                    .range(RedisConstant.SESSION_STATE_KEY_LIST, 0, -1);
+            if (CollectionUtils.isEmpty(sessionNameList)) {
+                return;
+            }
+            // 去重
+            Set<String> sessionNameSet = new HashSet<>(sessionNameList);
+            List<UnreadMessageNum> unreadMessageNumList = new ArrayList<>();
+            for (String sessionName : sessionNameSet) {
+                if (RedisUtil.hasRedisKey(sessionName)) {
+                    // 将数据存入 MySQL
+                    Map<Object, Object> dataMap = RedisUtil.STRING_REDIS_TEMPLATE.opsForHash().entries(sessionName);
+                    UnreadMessageNum unreadMessageNum = new UnreadMessageNum();
+                    unreadMessageNum.setId(Long.parseLong((String) dataMap.get("id")));
+                    unreadMessageNum.setUnreadNum(Integer.valueOf((String) dataMap.get("unreadNum")));
+                    unreadMessageNumList.add(unreadMessageNum);
+                }
+            }
+
+            // 更新 session name 的 key 列表
+            unreadMessageNumService.saveOrUpdateBatch(unreadMessageNumList, unreadMessageNumList.size());
+            RedisUtil.setListValue(RedisConstant.SESSION_STATE_KEY_LIST,
+                    new ArrayList<>(sessionNameSet),
+                    null,
+                    null);
+        };
+    }
+
+}
