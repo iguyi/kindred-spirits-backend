fbf970d13e0617c3e75652df349c5fc388278217
guyi <1802366955@qq.com>
2024-02-16 14:20:23 +0800

    扫描项目配置

--- src/main/java/com/guyi/kindredspirits/KindredSpiritsApplication.java
+++ src/main/java/com/guyi/kindredspirits/KindredSpiritsApplication.java
@@ -1,19 +1,23 @@
 package com.guyi.kindredspirits;
 
+import com.guyi.kindredspirits.common.ProjectProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 启动类
+ *
  * @author 孤诣
  */
 @SpringBootApplication
 @MapperScan("com.guyi.kindredspirits.mapper")
 @EnableScheduling
 @EnableAspectJAutoProxy(exposeProxy = true)
+@EnableConfigurationProperties(ProjectProperties.class)
 public class KindredSpiritsApplication {
 
     public static void main(String[] args) {
