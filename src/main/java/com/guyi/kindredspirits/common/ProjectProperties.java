b2ca027908b6e946afe41d947d76e0ae0242d965
guyi <1802366955@qq.com>
2024-02-16 14:20:03 +0800

    项目配置

---
+++ src/main/java/com/guyi/kindredspirits/common/ProjectProperties.java
@@ -0,0 +1,35 @@
+package com.guyi.kindredspirits.common;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * 项目配置
+ *
+ * @author 孤诣
+ */
+@ConfigurationProperties("project")
+@Data
+public class ProjectProperties {
+
+    /**
+     * 用户头像存放位置
+     */
+    private String userAvatarPath;
+
+    /**
+     * 队伍头像存放位置
+     */
+    private String teamAvatarPath;
+
+    /**
+     * 头像对应 url 前缀
+     */
+    private String urlPrefix;
+
+    /**
+     * 默认头像访问地址
+     */
+    private String defaultUserAvatarPath;
+
+}
