22917d5b67105fa326b0c59fc257925a5db82ec6
guyi <1802366955@qq.com>
2024-02-22 14:33:55 +0800

    格式化

--- src/main/java/com/guyi/kindredspirits/config/WebMvcConfig.java
+++ src/main/java/com/guyi/kindredspirits/config/WebMvcConfig.java
@@ -14,16 +14,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        //设置允许跨域的路径
+        // 设置允许跨域的路径
         registry.addMapping("/**")
-                //设置允许跨域请求的域名
-                //当**Credentials为true时，**Origin不能为星号，需为具体的ip地址【如果接口不带cookie,ip无需设成具体ip】
+                // 设置允许跨域请求的域名
+                // 当 ** Credentials 为 true 时, **Origin不能为星号, 需为具体的ip地址【如果接口不带cookie, ip无需设成具体ip】
+                // todo 上线
                 .allowedOrigins("http://127.0.0.1:3000", "http://localhost:3000")
-                //是否允许证书 不再默认开启
+                // 是否允许证书 不再默认开启
                 .allowCredentials(true)
                 //设置允许的方法
                 .allowedMethods("*")
-                //跨域允许时间
+                // 跨域允许时间
                 .maxAge(3600);
     }
 
