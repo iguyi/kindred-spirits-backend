aed1a0f71dc37a98f781b5857d87d98709ac8d2e
guyi <1802366955@qq.com>
2024-03-01 21:52:55 +0800

    补充注释

--- src/main/java/com/guyi/kindredspirits/job/SaveCacheJob.java
+++ src/main/java/com/guyi/kindredspirits/job/SaveCacheJob.java
@@ -8,6 +8,7 @@ import com.guyi.kindredspirits.util.redis.RedisUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
 import org.redisson.api.RedissonClient;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -65,42 +66,54 @@ public class SaveCacheJob {
     public Runnable saveSessionState() {
         return () -> {
             // 判断 key 是否存在
-            boolean hasRedisKey = RedisUtil.hasRedisKey(RedisConstant.SESSION_STATE_KEY_LIST);
+            String sessionStateKeyListKey = RedisConstant.SESSION_STATE_KEY_LIST;
+            boolean hasRedisKey = RedisUtil.hasRedisKey(sessionStateKeyListKey);
             if (!hasRedisKey) {
                 return;
             }
 
             // 获取所有未读(聊天)消息数据
-            List<String> sessionNameList = RedisUtil.STRING_REDIS_TEMPLATE.opsForList()
-                    .range(RedisConstant.SESSION_STATE_KEY_LIST, 0, -1);
+            StringRedisTemplate stringRedisTemplate = RedisUtil.STRING_REDIS_TEMPLATE;
+            List<String> sessionNameList = stringRedisTemplate.opsForList().range(sessionStateKeyListKey, 0, -1);
+
+            // 没有相关数据
             if (CollectionUtils.isEmpty(sessionNameList)) {
                 return;
             }
-            // 去重
+
+            // 对数据进行处理: 处理为正确格式、去重
             Set<String> sessionNameSet = sessionNameList.stream()
                     .map(sessionName -> sessionName.replace("\"", ""))
                     .collect(Collectors.toSet());
+
+            // 收集需要保存的数据
             List<UnreadMessageNum> unreadMessageNumList = new ArrayList<>();
             for (String sessionName : sessionNameSet) {
+                // 将数据存入 MySQL
                 if (RedisUtil.hasRedisKey(sessionName)) {
-                    // 将数据存入 MySQL
-                    Map<Object, Object> dataMap = RedisUtil.STRING_REDIS_TEMPLATE.opsForHash().entries(sessionName);
-                    // 避免不必要的数据更新
+                    // 从缓存中获取对应数据
+                    Map<Object, Object> dataMap = stringRedisTemplate.opsForHash().entries(sessionName);
+
+                    // 避免不必要的数据更新(在最近更新过的数据)
                     if (OLD_SESSION_STATE.containsKey(sessionName)) {
                         if (Objects.equals(OLD_SESSION_STATE.get(sessionName), dataMap)) {
                             continue;
                         }
                     }
+
+                    // 组合需要更新到 MySQL 的数据
                     UnreadMessageNum unreadMessageNum = new UnreadMessageNum();
                     unreadMessageNum.setId(Long.parseLong((String) dataMap.get("id")));
                     unreadMessageNum.setUnreadNum(Integer.valueOf((String) dataMap.get("unreadNum")));
                     unreadMessageNumList.add(unreadMessageNum);
+
+                    // 记录
                     OLD_SESSION_STATE.put(sessionName, dataMap);
                 }
             }
 
+            // 没有需要保存的数据
             if (unreadMessageNumList.isEmpty()) {
-                // 没有需要保存的数据
                 return;
             }
 
@@ -108,10 +121,10 @@ public class SaveCacheJob {
             unreadMessageNumService.saveOrUpdateBatch(unreadMessageNumList, unreadMessageNumList.size());
 
             // 更新 session name 的 key 列表
-            Boolean result = RedisUtil.STRING_REDIS_TEMPLATE.delete(RedisConstant.SESSION_STATE_KEY_LIST);
+            Boolean result = stringRedisTemplate.delete(sessionStateKeyListKey);
             result = Optional.ofNullable(result).orElse(false);
             if (result) {
-                RedisUtil.setListValue(RedisConstant.SESSION_STATE_KEY_LIST,
+                RedisUtil.setListValue(sessionStateKeyListKey,
                         new ArrayList<>(sessionNameSet),
                         null,
                         null);
